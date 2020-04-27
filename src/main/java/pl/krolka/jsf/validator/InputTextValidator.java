70dbe46a6a2b8a9ad9c38ac73fdbc4b3ad6fdca3
Krzysztof Rolka <krzysztofrolka@op.pl>
2020-04-27 18:43:58 +0200

    Add Client and Server side validation

---
+++ src/main/java/pl/krolka/jsf/validator/InputTextValidator.java
@@ -0,0 +1,43 @@
+package pl.krolka.jsf.validator;
+
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value = "inputTextValidator")
+public class InputTextValidator implements Validator<Object> {
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+		String inputText = value.toString();
+		FacesMessage message = null;
+		
+		try {
+			
+			//String length more then 4 char.
+			if (inputText.length() <= 3) {
+				message = new FacesMessage("Pole nie powinno byc krótsze niz 3 znaki.");
+			}
+			
+			// String must contain only letters, numbers,polish signs, space and dash
+			if (!Pattern.matches("^[0-9a-zA-ZąĄęĘóÓśŚłŁżŻźŹćĆńŃ -]*",inputText)) {
+				message = new FacesMessage("Pole nie może zawierać znaków specjalnych.");
+			}
+			
+			if (message != null && !message.getDetail().isEmpty()) {
+				throw new ValidatorException(message);
+			}
+
+		} catch (Exception ex) {
+			throw new ValidatorException(new FacesMessage(ex.getMessage()));
+		}
+
+	}
+
+}
